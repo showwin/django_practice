4eee2ee6ea2bde4d8953c64ff51dfae91e905dbb
showwin <showwin_kmc@yahoo.co.jp>
2016-11-21 09:24:32 +0900

    tutorial01

--- django_practice/urls.py
+++ django_practice/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
+# URLパターンをインクルードするときには常に `include` を使う
+# `admin.site.urls` は唯一の例外
 urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
